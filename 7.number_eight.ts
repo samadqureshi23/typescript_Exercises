192f2ff4d38184eb12aa3c1b135f9763df939d43
samadqureshi23 <162016414+samadqureshi23@users.noreply.github.com>
2024-04-04 03:27:50 +0500

    Add files via upload

---
+++ 7.number_eight.ts
@@ -0,0 +1,7 @@
+console.log(6+2); // ADDITION
+
+console.log(10-2); // SUBTRACTION
+
+console.log(4*2); // MILTIPLICASION
+
+console.log(16/2); // DIVIDE
---
+++ 9.program_1.ts
@@ -0,0 +1,8 @@
+
+
+// Program written by: samad
+// current Date: 13-03-2024
+
+//this program will write hello world
+
+console.log('Hello world');
